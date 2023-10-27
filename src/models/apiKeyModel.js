21b034ace2772bd34fcc351b826bbea76ccf70b5
Fiattarone <Fiattarone@me.com>
2023-10-26 20:55:09 -0700

    Update apiKeySchema

--- src/models/apiKeyModel.js
+++ src/models/apiKeyModel.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const apiKeySchema = new mongoose.Schema(
 	{
 		apiKey: { type: String, unique: true },
+		institutionName: { type: String }, // make required: true
+		contactEmail: { type: String }, // make required: true
+		issuedBy: { type: String }, // make required: true
+		issuedAt: { type: Date, default: Date.now },
+		isActive: { type: Boolean, default: true },
+		usageStats: {
+			totalRequests: { type: Number, default: 0 },
+			lastUsed: { type: Date },
+		},
 	},
 	{
 		collection: 'apiKey',
